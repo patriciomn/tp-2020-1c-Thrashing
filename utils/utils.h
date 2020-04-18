22eeb5ceeb77932939d72e0803d7e0c7ecd871eb
Tomas <tomasortiz9598@gmail.com>
2020-04-18 18:04:19 -0300

    Add utils & broker folder. remove .log en gitignore

---
+++ utils/utils.h
@@ -0,0 +1,28 @@
+typedef struct
+{
+	int size;
+	void* stream;
+} t_buffer;
+
+typedef struct
+{
+	enum TIPO codigo_operacion;
+	t_buffer* buffer;
+} t_paquete;
+
+
+typedef struct{
+	char* name;
+	int posx;
+	int posy;
+}pokemon;
+
+enum TIPO{
+	NEW_POKEMON = 1,
+	APPEARED_POKEMON = 2,
+	CATCH_POKEMON = 3,
+	CAUGHT_POKEMON = 4,
+	GET_POKEMON = 5,
+	LOCALIZED_POKEMON = 6,
+	SUSCRITO = 7,
+};
