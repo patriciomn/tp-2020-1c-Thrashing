22402152e13b2226c43df73b2d638a9ac6a3cc3a
Tomas <tomasortiz9598@gmail.com>
2020-04-23 12:17:23 -0300

    aasdd

---
+++ makefile
@@ -0,0 +1,6 @@
+all: 
+	+$(MAKE) -C team
+	+$(MAKE) -C gameboy
+	+$(MAKE) -C broker
+	+$(MAKE) -C game-card
+	
---
+++ utils/utils.c
@@ -0,0 +1,8 @@
+#include "utils.h"
+t_log* iniciar_logger(const char * process){
+	return log_create("gameboy.log",process,1,LOG_LEVEL_INFO);
+}
+
+t_config* leer_config(void){
+	return config_create("gameboy.config");
+}
