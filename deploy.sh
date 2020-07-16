045bfc83267516193e591c4a6e8f300254f22bb9
Tomas Ortiz <tomasortiz9598@gmail.com>
2020-07-16 15:37:40 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/sisoputnfrba/so-commons-library.git 
 cd so-commons-library
-make install
+sudo make install
 cd ..
 git clone https://github.com/sisoputnfrba/tp-2020-1c-Thrashing.git 
 cd tp-2020-1c-Thrashing
